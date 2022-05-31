095a5358074ebf2d74e1c6d5ae7ac5327de1ca76
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-05-31 09:13:37 -0700

    adds tictactoe game board

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('js is working')
+/*----- constants -----*/
+const MAX_PLAYS = 9
+const ALLOWED_GUESSES = ['X', 'O']
+
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+function initGame() {
+
+}
+
